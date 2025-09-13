f81684cd53836476799e42de0c637fb97d1a9972
devheshmati <reza2boyce@gmail.com>
2025-09-14 02:15:39 +0330

    initial project commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+// list of directives // v-bind // v-model // v-on or @: or @ // v-if // v-else
+// v-show // v-for // computed // watch // watchEffect // composition function
+// props -- send data to the child component // emit events -- send data to the
+parent // slots // provide and inject // router // nested routes | params |
+query string // pinia state manager | store | actions: if you need update state
+| getters: if you don't need update state // axios | axios get | axios post //
+hook lifecycle // forms // login && signup // navigation guards // form
+validation | vee-validate // lazy loading
+
+<script setup></script>
+
+<template></template>
+
+<style></style>
+
---
+++ src/router/router.js
@@ -0,0 +1,21 @@
+import Home from "../views/Home.vue";
+import Login from "../views/Login.vue";
+import Register from "../views/Register.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: Login,
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: Register,
+  },
+];
