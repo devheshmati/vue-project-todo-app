0df372d40b2ce613c5fc0a972e50e9981e2aabc8
devheshmati <reza2boyce@gmail.com>
2025-10-09 14:12:06 +0330

    split frontend and backend

---
+++ backend/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'womoboy/backend',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '969432941a5069deb71dec844036d03fb367fdb2',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'womoboy/backend' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '969432941a5069deb71dec844036d03fb367fdb2',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
