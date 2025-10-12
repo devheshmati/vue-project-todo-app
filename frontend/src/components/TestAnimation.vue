63cf48e85cf2088ce09f1344170de8fb10e04081
devheshmati <reza2boyce@gmail.com>
2025-10-12 22:15:05 +0330

    some SplitText plugin for gsap training

--- frontend/src/components/TestAnimation.vue
+++ frontend/src/components/TestAnimation.vue
@@ -1,54 +1,42 @@
 <script setup>
 import { gsap } from "gsap";
 import { SplitText } from "gsap/SplitText";
-import { onMounted } from "vue";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { onMounted, ref } from "vue";
 
-gsap.registerPlugin(SplitText);
+gsap.registerPlugin(SplitText, ScrollTrigger);
+
+let split = ref(null);
 
 onMounted(() => {
   document.fonts.ready.then(() => {
-    // txt1
-    gsap.set(".txt1", { opacity: 1 });
-
-    let split;
     SplitText.create(".txt1", {
-      type: "words, lines",
+      type: "lines",
       autoSplit: true,
       mask: "lines",
       onSplit: (self) => {
-        split = gsap.from(self.lines, {
+        split.value = gsap.from(self.lines, {
+          opacity: 0,
           duration: 0.6,
           yPercent: 100,
-          opacity: 0,
           stagger: 0.1,
           ease: "expo.out",
         });
-
-        return split;
       },
-    });
-
-    // txt2
-    let split2 = SplitText.create(".txt2", {
-      type: "lines, words, chars",
-      mask: "lines",
-      autoSplit: true,
-    });
-
-    gsap.from(split2.lines, {
-      duration: 0.6,
-      yPercent: 100,
-      opacity: 0,
-      stagger: 0.1,
-      ease: "expo.out",
     });
   });
 });
+
+function handleClick() {
+  if (split.value) {
+    split.value.play(0);
+  }
+}
 </script>
 
 <template>
   <div>
-    <p class="txt1 w-1/2 mx-auto mt-10">
+    <p class="txt1 w-1/2 mx-auto mt-10 text-xl">
       Lorem Ipsum is simply dummy text of the printing and typesetting industry.
       Lorem Ipsum has been the industry's standard dummy text ever since the
       1500s, when an unknown printer took a galley of type and scrambled it to
@@ -72,6 +60,15 @@ onMounted(() => {
       publishing software like Aldus PageMaker including versions of Lorem
       Ipsum.
     </p>
+  </div>
+
+  <div>
+    <button
+      @click="handleClick"
+      class="bg-purple-700 text-white px-4 py-2 mx-auto"
+    >
+      Run Animation Again
+    </button>
   </div>
 </template>
 
