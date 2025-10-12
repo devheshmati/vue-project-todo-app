693775dccc9f82ca063a6bf8161a392be383ee27
devheshmati <reza2boyce@gmail.com>
2025-10-12 13:34:44 +0330

    training for some funny animation with split text gsap plugin

--- frontend/src/components/TestAnimation.vue
+++ frontend/src/components/TestAnimation.vue
@@ -1,31 +1,44 @@
 <script setup>
-import { onMounted } from "vue";
 import { gsap } from "gsap";
+import { SplitText } from "gsap/SplitText";
+import { onMounted } from "vue";
+
+gsap.registerPlugin(SplitText);
 
 onMounted(() => {
-  const tl = gsap.timeline();
-  const anime1 = {
-    scale: 0.1,
-    duration: 1,
-    stagger: { amount: 1, from: "center", grid: "auto" },
+  let splitText1 = SplitText.create(".txt1", {
+    type: "chars, words, lines",
+  });
+
+  gsap.from(splitText1.words, {
+    yPercent: "random([-100, 100])",
+    rotation: "random(-30, 30)",
+    ease: "power3.out",
+    autoAlpha: 0,
     repeat: -1,
     yoyo: true,
-    opacity: 1,
-  };
-  tl.to(".box", anime1);
+    stagger: {
+      amount: 1,
+      from: "random",
+    },
+  });
 });
 </script>
 
 <template>
   <div>
-    <ul
-      class="grid grid-cols-12 gap-2 bg-gray-700 p-2 justify-items-center h-screen items-center overflow-hidden"
-    >
-      <li
-        v-for="x in 72"
-        :key="x"
-        class="w-[90px] h-[90px] bg-red-600 box flex justify-center items-center text-white"
-      ></li>
-    </ul>
+    <p class="txt1 w-1/2 mx-auto mt-10">
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the
+      1500s, when an unknown printer took a galley of type and scrambled it to
+      make a type specimen book. It has survived not only five centuries, but
+      also the leap into electronic typesetting, remaining essentially
+      unchanged. It was popularised in the 1960s with the release of Letraset
+      sheets containing Lorem Ipsum passages, and more recently with desktop
+      publishing software like Aldus PageMaker including versions of Lorem
+      Ipsum.
+    </p>
   </div>
 </template>
+
+<style scoped></style>
