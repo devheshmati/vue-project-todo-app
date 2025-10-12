b0295221af3aa74d9f12100aa9c727ccdceff21b
devheshmati <reza2boyce@gmail.com>
2025-10-12 15:57:33 +0330

    some splitText training about masking

--- frontend/src/components/TestAnimation.vue
+++ frontend/src/components/TestAnimation.vue
@@ -6,21 +6,42 @@ import { onMounted } from "vue";
 gsap.registerPlugin(SplitText);
 
 onMounted(() => {
-  let splitText1 = SplitText.create(".txt1", {
-    type: "chars, words, lines",
-  });
+  document.fonts.ready.then(() => {
+    // txt1
+    gsap.set(".txt1", { opacity: 1 });
 
-  gsap.from(splitText1.words, {
-    yPercent: "random([-100, 100])",
-    rotation: "random(-30, 30)",
-    ease: "power3.out",
-    autoAlpha: 0,
-    repeat: -1,
-    yoyo: true,
-    stagger: {
-      amount: 1,
-      from: "random",
-    },
+    let split;
+    SplitText.create(".txt1", {
+      type: "words, lines",
+      autoSplit: true,
+      mask: "lines",
+      onSplit: (self) => {
+        split = gsap.from(self.lines, {
+          duration: 0.6,
+          yPercent: 100,
+          opacity: 0,
+          stagger: 0.1,
+          ease: "expo.out",
+        });
+
+        return split;
+      },
+    });
+
+    // txt2
+    let split2 = SplitText.create(".txt2", {
+      type: "lines, words, chars",
+      mask: "lines",
+      autoSplit: true,
+    });
+
+    gsap.from(split2.lines, {
+      duration: 0.6,
+      yPercent: 100,
+      opacity: 0,
+      stagger: 0.1,
+      ease: "expo.out",
+    });
   });
 });
 </script>
@@ -28,6 +49,19 @@ onMounted(() => {
 <template>
   <div>
     <p class="txt1 w-1/2 mx-auto mt-10">
+      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+      Lorem Ipsum has been the industry's standard dummy text ever since the
+      1500s, when an unknown printer took a galley of type and scrambled it to
+      make a type specimen book. It has survived not only five centuries, but
+      also the leap into electronic typesetting, remaining essentially
+      unchanged. It was popularised in the 1960s with the release of Letraset
+      sheets containing Lorem Ipsum passages, and more recently with desktop
+      publishing software like Aldus PageMaker including versions of Lorem
+      Ipsum.
+    </p>
+  </div>
+  <div>
+    <p class="txt2 w-1/2 mx-auto mt-10">
       Lorem Ipsum is simply dummy text of the printing and typesetting industry.
       Lorem Ipsum has been the industry's standard dummy text ever since the
       1500s, when an unknown printer took a galley of type and scrambled it to
