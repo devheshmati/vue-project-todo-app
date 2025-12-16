20e3548e5aa900edc8b56eec9926a31514de2231
devheshmati <reza2boyce@gmail.com>
2025-12-16 10:12:37 -0500

    fix todo list height problem

--- frontend/src/components/TodosList.vue
+++ frontend/src/components/TodosList.vue
@@ -221,20 +221,21 @@ watch(
     <div
       ref="mainFrame"
       v-if="todoStore.todosList.length > 0 && !todoStore.errorMessage"
-      class="w-9/10 sm:w-3/4 sm:min-w-2/5 md:w-1/2 bg-gray-400 rounded-lg mx-auto justify-items-center h-8/10"
+      class="w-9/10 sm:w-3/4 sm:min-w-2/5 md:w-1/2 bg-gray-400 rounded-lg mx-auto justify-items-center overflow-hidden"
     >
       <!-- main header -->
-      <div class="bg-gray-800 p-4 col-span-1 w-full h-fit">
-        <h3 ref="mainHeader" class="text-xl font-bold text-center text-white">
-          Todo List
-        </h3>
-      </div>
+      <h3
+        ref="mainHeader"
+        class="text-xl font-bold text-center text-white bg-black py-2 w-full"
+      >
+        Todo List
+      </h3>
 
-      <div class="w-9/10 py-2">
-        <!-- sort section -->
+      <!-- sort section -->
+      <div class="w-full p-2">
         <div
           ref="mainSortFrame"
-          class="flex flex-col sm:flex-row gap-2 justify-between p-2 bg-gray-200 rounded-xl text-md sm:text-sm w-full h-fit"
+          class="flex flex-col sm:flex-row gap-2 justify-between p-2 bg-gray-200 rounded-xl text-md sm:text-sm w-full"
         >
           <div>
             <label for="#todoSortByEl" class="text-gray-700">Sort by: </label>
@@ -258,98 +259,98 @@ watch(
             </select>
           </div>
         </div>
+      </div>
 
-        <div class="py-2 h-full">
-          <div ref="mainTodoListParent" class="w-full overflow-y-auto h-98">
-            <ul
-              ref="mainTodoList"
-              class="flex flex-col gap-2"
-              :class="{ 'pe-2': hasScrollbar }"
+      <div class="p-2 w-full">
+        <div ref="mainTodoListParent" class="w-full overflow-y-auto h-90">
+          <ul
+            ref="mainTodoList"
+            class="flex flex-col gap-2 list-disc"
+            :class="{ 'pe-2': hasScrollbar }"
+          >
+            <li
+              ref="todoItems"
+              v-for="todo in filteredAndSortedTodos"
+              :key="todo.id"
+              class="grid grid-cols-6 p-4 w-full h-fit rounded-lg"
+              :class="todo.is_done ? 'bg-gray-300' : 'bg-white'"
             >
-              <li
-                ref="todoItems"
-                v-for="todo in filteredAndSortedTodos"
-                :key="todo.id"
-                class="grid grid-cols-6 p-4 w-full h-fit rounded-lg"
-                :class="todo.is_done ? 'bg-gray-300' : 'bg-white'"
-              >
-                <div class="col-span-4">
-                  <span :class="todo.is_done ? 'text-slate-600' : ''">{{
-                    todo.title
-                  }}</span>
-                </div>
-                <div class="col-span-2 flex justify-end gap-2">
-                  <!-- Check -->
-                  <button
-                    class="cursor-pointer rounded-md"
-                    :class="todo.is_done ? 'bg-slate-400' : 'bg-slate-300'"
-                    @click="todoStore.updateTodoStatus(todo)"
+              <div class="col-span-4">
+                <span :class="todo.is_done ? 'text-slate-600' : ''">{{
+                  todo.title
+                }}</span>
+              </div>
+              <div class="col-span-2 flex justify-end gap-2">
+                <!-- Check -->
+                <button
+                  class="cursor-pointer rounded-md"
+                  :class="todo.is_done ? 'bg-slate-400' : 'bg-slate-300'"
+                  @click="todoStore.updateTodoStatus(todo)"
+                >
+                  <input
+                    type="checkbox"
+                    :checked="todo.is_done"
+                    class="peer sr-only"
+                  />
+                  <svg
+                    class="w-6 h-6 text-transparent peer-checked:text-slate-700 transition-colors duration-200"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2.5"
+                    viewBox="0 0 24 24"
                   >
-                    <input
-                      type="checkbox"
-                      :checked="todo.is_done"
-                      class="peer sr-only"
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M4.5 12.75l6 6 9-13.5"
                     />
-                    <svg
-                      class="w-6 h-6 text-transparent peer-checked:text-slate-700 transition-colors duration-200"
-                      fill="none"
-                      stroke="currentColor"
-                      stroke-width="2.5"
-                      viewBox="0 0 24 24"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="M4.5 12.75l6 6 9-13.5"
-                      />
-                    </svg>
-                  </button>
+                  </svg>
+                </button>
 
-                  <!--Update -->
-                  <button
-                    class="cursor-pointer text-slate-600 hover:text-amber-500"
-                    @click="openEditModal(todo)"
+                <!--Update -->
+                <button
+                  class="cursor-pointer text-slate-600 hover:text-amber-500"
+                  @click="openEditModal(todo)"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="size-6"
                   >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      class="size-6"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125"
-                      />
-                    </svg>
-                  </button>
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125"
+                    />
+                  </svg>
+                </button>
 
-                  <!-- Delete -->
-                  <button
-                    class="cursor-pointer text-slate-600 hover:text-red-500"
-                    @click="todoStore.deleteTodo(todo.id)"
+                <!-- Delete -->
+                <button
+                  class="cursor-pointer text-slate-600 hover:text-red-500"
+                  @click="todoStore.deleteTodo(todo.id)"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="size-6"
                   >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke-width="1.5"
-                      stroke="currentColor"
-                      class="size-6"
-                    >
-                      <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                      />
-                    </svg>
-                  </button>
-                </div>
-              </li>
-            </ul>
-          </div>
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
